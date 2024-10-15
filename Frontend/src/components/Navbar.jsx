6bc7f89384dcf66059b0ff582422251349d1815d
Qaiem <qaiem575@gmail.com>
2024-10-15 10:55:19 +0500

    Day 2 of Working With Ecommerce Website

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -46,8 +46,15 @@ const Navbar = () => {
      </div>
      {/* {Side Menu For Small Screen} */}
      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
-             <div className="flex flex-col">
-
+             <div className="flex flex-col text-gray-600">
+                <div onClick={()=>setvisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
+                   <img className="h-4 rotaye-180" src={assets.dropdown_icon} alt="" />
+                   <p>Back</p>
+                </div>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/collection' >COLLECTION</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
              </div>
      </div>      
     </div>
